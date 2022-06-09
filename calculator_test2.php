4775a308dd57f2ce8f3603a42ce23b21cb4ad06d
Josh Lee <103002260+hsojeel@users.noreply.github.com>
2022-06-09 16:32:50 +0000

    Adds subtraction test.php

--- calculator_test2.php
+++ calculator_test2.php
@@ -1,10 +1,27 @@
 <?php
-//divide-by-zero.php
+//calculator_test2.php
 require_once 'simpletest/autorun.php';
 include 'Calculator.php';
+
 class TestOfCalculator extends UnitTestCase {
-    function testDivide() {
-      $myCalc = new Calculator();
-      $this->assertEqual($myCalc->divide(3,0), "ERR", "Divide by zero, returns ERR");
-    }
+	function testAdd() {
+	  $myCalc = new Calculator();
+	  $this->assertEqual($myCalc->add(1,1), 2, "1 + 1");
+	  $this->assertEqual($myCalc->add(2,2), 4, "2 + 2");
+	  $this->assertEqual($myCalc->add(1,1,1), 3, "three params, 1 + 1 + 1");
+	}
+	
+	function testMultiply() {
+	  $myCalc = new Calculator();
+	  $this->assertEqual($myCalc->multiply(1,1), 1, "1 * 1");
+	  $this->assertEqual($myCalc->multiply(2,2), 4, "2 * 2");
+	  $this->assertEqual($myCalc->multiply(3,5), 15, "3 * 5");
+	}
+	
+	function testDivide() {
+	  $myCalc = new Calculator();
+	  $this->assertEqual($myCalc->divide(1,1), 1, "1 / 1");
+	  $this->assertEqual($myCalc->divide(2,2), 1, "2 / 2");
+	  $this->assertEqual($myCalc->divide(3,0), 15, "3 / 0");
+	}
 }
